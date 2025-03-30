4ea09f6cd560cd12281f023f8c8e7ebd79a1fb5c
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2025-03-30 17:28:31 +0530

    git pushed

--- src/app/test/testget/page.tsx
+++ src/app/test/testget/page.tsx
@@ -1,20 +1,34 @@
 "use client"
 import { api } from '@/trpc/react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const page = () => {
    
 
    const utils = api.useUtils();
      const [name, setName] = useState("");
-     const { data:latestPost } = api.post.getAllPapers.useQuery(
-       "cm8vc8tsj0003m50mc49yukds",
-     );
-     console.log(latestPost, "latestPost")
-   
+    //  const [data,setData] = useState();
+     
+    //  useEffect(() => {
+    //    const fetchData = async () => {
+    //      try {
+    //        const response = await fetch(
+    //          `/api/getPaper/cm8vc8tsj0003m50mc49yukds`,
+    //        );
+    //        const data = await response.json();
+    //        console.log(data);
+    //         setData(data);
+    //      } catch (error) {
+    //        console.error("Error fetching hospital data:", error);
+    //      }
+    //    };
+    //    fetchData();
+    //  }, []);
+    const {data}=api.post.getAllPapers.useQuery("cm8vc8tsj0003m50mc49yukds");
+    console.log(data, "examData")
      return (
        <div className="w-full max-w-xs">
-         {latestPost ? (
+         {data ? (
            <p className="truncate">Your most recent post</p>
          ) : (
            <p>You have no posts yet.</p>
